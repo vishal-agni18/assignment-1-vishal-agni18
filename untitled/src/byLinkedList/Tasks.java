2445a2139c7c64eb0a2f4964ae84950e13fe0af3
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 18:37:34 +0530

    create method addContact

--- untitled/src/byLinkedList/Tasks.java
+++ untitled/src/byLinkedList/Tasks.java
@@ -1,5 +1,72 @@
 package byLinkedList;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Tasks {
+    Scanner scanner = new Scanner(System.in);
+    LinkedList<String> linkedList = new LinkedList<>();
+
+    public void addContact(LinkedList1 linkedList1) {
+        System.out.println("You have chosen to add a new contact: \n" +
+                "Please enter the name of the Person");
+        System.out.println("First Name : ");
+        String firstName = scanner.nextLine().trim();
+        while (true) {
+            if (firstName.matches("[A-Za-z]+")) {
+                break;
+            } else {
+                System.out.println("Please enter correct name");
+            }
+            firstName = scanner.nextLine().trim();
+        }
+        System.out.println("Last Name : ");
+        String lastName = scanner.nextLine().trim();
+        while (true) {
+            if (lastName.matches("[A-Za-z]+")) {
+                break;
+            } else {
+                System.out.println("Please enter correct name");
+            }
+            lastName = scanner.nextLine().trim();
+        }
+        System.out.println("Contact Number:");
+        while (true) {
+            String contact = scanner.nextLine().trim();
+            if (contact.matches("[0-9]{10}")) {
+                linkedList.add(contact);
+                break;
+            }
+        }
+        while (true) {
+            System.out.println("Would you like to add another contact number? (y/n):");
+            char choice = scanner.next().charAt(0);
+            if (choice == 'y') {
+                System.out.println("Conatct Number : ");
+                String contactNumber1 = scanner.nextLine().trim();
+                while (true) {
+                    if (contactNumber1.matches("[0-9]{10}")) {
+                        linkedList.add(contactNumber1);
+                        break;
+                    } else {
+                        System.out.println("Add correct number");
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println("Enter your email address:");
+        scanner.nextLine();
+        while (true) {
+            String temp = scanner.nextLine();
+            if (temp.matches("^[a-z]+[0-9]*@[a-z]+\\.[a-z]+$")) {
+                linkedList1.addLast(new Person(firstName, lastName, linkedList, temp));
+                break;
+            } else {
+                System.out.println("Invalid email address ! Enter again");
+            }
+        }
+    }
 
 }
