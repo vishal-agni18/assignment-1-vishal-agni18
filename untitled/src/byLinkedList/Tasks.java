e9a8f5d1e4631418938bbab0703a07ece2f25618
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 18:49:31 +0530

    create method searchContact to search contact in contact list

--- untitled/src/byLinkedList/Tasks.java
+++ untitled/src/byLinkedList/Tasks.java
@@ -93,4 +93,16 @@ public class Tasks {
         }
     }
 
+    public void searchContact(LinkedList1 linkedList1) {
+        System.out.println("You could search for a contact from their first names: ");
+        String firstName = scanner.nextLine().trim();
+        if (linkedList1.indexOf(firstName) >= 0) {
+            System.out.println("Match found");
+            linkedList1.getNode(linkedList1.indexOf(firstName));
+        } else {
+            System.out.println("No match found");
+        }
+    }
+
+
 }
