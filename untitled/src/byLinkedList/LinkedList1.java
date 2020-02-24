509cdc89356b8d37d8ab61d44f1aa00fd87f5275
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 18:03:38 +0530

    create a method addlast

--- untitled/src/byLinkedList/LinkedList1.java
+++ untitled/src/byLinkedList/LinkedList1.java
@@ -5,4 +5,24 @@ public class LinkedList1 {
     private Node first;
     private Node last;
 
+    public void addLast(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            last.setNext(node);
+            last = node;
+        }
+    }
+
+    public boolean isEmpty() {
+        boolean response = false;
+        if (first == null) {
+            response = true;
+        }
+        return response;
+    }
+
+
 }
--- untitled/src/byLinkedList/Tasks.java
+++ untitled/src/byLinkedList/Tasks.java
@@ -1,4 +1,5 @@
 package byLinkedList;
 
 public class Tasks {
+
 }
