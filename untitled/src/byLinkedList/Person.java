76b7ceb75bbbea2255ae50e02691fed206b2168f
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 17:29:53 +0530

    make getter setter

--- untitled/src/byLinkedList/Person.java
+++ untitled/src/byLinkedList/Person.java
@@ -15,5 +15,35 @@ public class Person {
         this.email = email;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LinkedList<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(LinkedList<String> contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
