bd72184b0a02f7b2a953a4d68e3b88eddf9e3f9d
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 17:39:07 +0530

    create getter setter

--- untitled/src/byLinkedList/Node.java
+++ untitled/src/byLinkedList/Node.java
@@ -7,4 +7,20 @@ public class Node {
     public Node(Person value) {
         this.value = value;
     }
+
+    public Person getValue() {
+        return value;
+    }
+
+    public void setValue(Person value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
