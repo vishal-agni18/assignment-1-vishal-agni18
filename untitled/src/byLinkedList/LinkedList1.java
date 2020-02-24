1a1e41a1923e9cccb46319ce78ca650402b88fea
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 18:12:29 +0530

    create a method removefirst to delete element from first position

--- untitled/src/byLinkedList/LinkedList1.java
+++ untitled/src/byLinkedList/LinkedList1.java
@@ -47,6 +47,16 @@ public class LinkedList1 {
         }
         return -1;
     }
+    public void removeFirst() {
+        if (first == last) {
+            first = null;
+            last = null;
+        } else {
+            Node second = first.getNext();
+            first = second;
+        }
+    }
+
 
 
 
