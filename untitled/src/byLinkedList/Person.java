8fe8f59ea4c2e89d7c35d5d8d327773b72a5631d
This PC <vishal.agnihotri_cvl18@gla.ac.in>
2020-02-24 17:27:28 +0530

    make attributes private

--- untitled/src/byLinkedList/Person.java
+++ untitled/src/byLinkedList/Person.java
@@ -3,6 +3,17 @@ package byLinkedList;
 import java.util.LinkedList;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private LinkedList<String> contacts;
+    private String email;
+
+    public Person(String firstName, String lastName, LinkedList<String> contacts, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contacts = contacts;
+        this.email = email;
+    }
 
 
 }
